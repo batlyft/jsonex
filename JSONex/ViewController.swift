b8a37a10a7befa824e747c4fc4ad02d399323f1e
Bat Computer <Batcomputer@Bats-MacBook-Pro.local>
2015-12-30 10:29:32 -0800

    first commit

---
+++ JSONex/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  JSONex
+//
+//  Created by Bat Computer on 11/11/15.
+//  Copyright © 2015 HardSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = NSURL(string: "http://www.telize.com/geoip")!
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+            
+            if let urlContent = data {
+                
+                do {
+                
+               let jsonResult = try NSJSONSerialization.JSONObjectWithData(urlContent, options: NSJSONReadingOptions.MutableContainers)
+                    
+                    print(jsonResult["city"])
+                
+                } catch {
+                    
+                    print("JSON serialization failed")
+                    
+                }
+                
+            }
+            
+        }
+        
+        task.resume()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
